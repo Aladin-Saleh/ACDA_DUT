329e94f492632292b53d5d6b845334d1b0a5d2d5
saleh <saleh@localhost>
2020-09-17 13:54:23 +0000

    a finir

--- TP/TPACDA/exo3/Souris.java
+++ TP/TPACDA/exo3/Souris.java
@@ -11,15 +11,21 @@ public class Souris implements MouseListener
 
 private Screen fen;
 private JLabel img;
-private String photo[] = new String[2];
+private String photo[] = new String[6];
 private int i = 0;
+private int indice = 0;
 
 public Souris(Screen fen,JLabel img)
 {	
 	this.fen = fen;
 	this.img = img;
-photo[0] = new String("singe.jpg");
-photo[1] = "yoda.jpg";
+
+for (i = 0; i < photo.length;i++ ) {
+    
+    photo[i] = i+".jpg";
+
+
+}
 
 	
 
@@ -28,13 +34,17 @@ photo[1] = "yoda.jpg";
 
    @Override
         public void mouseClicked(MouseEvent event) {
+         
              System.out.println("X : " + event.getX() +  " Y : " + event.getY());
-              fen.remove(img);
+          
 
-             JLabel image = new JLabel(new ImageIcon(photo[i]));
-             
+             fen.remove(img);
+             img.setOpaque(true);
+
+             JLabel image = new JLabel(new ImageIcon(photo[++indice%this.photo.length]));
              fen.add(image);
-			 fen.revalidate();
+			       fen.revalidate();
+
 			 
           	          	    
 
