3a91eabad7c9c7cda1dc697db5e6070faa8a5afc
saleh <saleh@localhost>
2020-09-17 13:22:55 +0000

    a finir

--- TP/TPACDA/exo3/Screen.java
+++ TP/TPACDA/exo3/Screen.java
@@ -8,21 +8,30 @@ import javax.swing.ImageIcon;
 public class Screen extends JFrame  
 {
 
-
+	
 	public Screen()
 	{
+
 		this.setSize(500,500);
 		this.setLocation(100,100);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JLabel image = new JLabel(new ImageIcon("index.jpg"));
-		this.add(image);
-		JLabel image2 = new JLabel(new ImageIcon("yoda.jpg"));
-		this.add(image2);
-		JLabel image3 = new JLabel(new ImageIcon("index.jpg"));
-		this.add(image3);
+		
 
-		Souris souris = new Souris(this);
+
+		JLabel image = new JLabel(new ImageIcon("index.jpg"));
+		
+
+
+	
+
+		this.add(image);
+		
+
+
+
+
+		Souris souris = new Souris(this,image);
 		this.addMouseListener(souris);
 
 		this.setVisible(true);
--- TP/TPACDA/exo3/Souris.java
+++ TP/TPACDA/exo3/Souris.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.JLabel; 
+import javax.swing.ImageIcon; 
 
 
 
@@ -9,18 +11,32 @@ public class Souris implements MouseListener
 
 private Screen fen;
 private JLabel img;
+private String photo[] = new String[2];
+private int i = 0;
 
 public Souris(Screen fen,JLabel img)
-{
+{	
 	this.fen = fen;
 	this.img = img;
+photo[0] = new String("singe.jpg");
+photo[1] = "yoda.jpg";
+
+	
+
 }
+
 
    @Override
         public void mouseClicked(MouseEvent event) {
              System.out.println("X : " + event.getX() +  " Y : " + event.getY());
-             fen.remove(img);
-             fen.revalidate();
+              fen.remove(img);
+
+             JLabel image = new JLabel(new ImageIcon(photo[i]));
+             
+             fen.add(image);
+			 fen.revalidate();
+			 
+          	          	    
 
         }
   
