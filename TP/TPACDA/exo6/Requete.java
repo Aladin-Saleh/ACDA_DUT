8acddbdafc56bbd7cbad9a85e2b4197cf78039af
saleh <saleh@localhost>
2020-09-22 14:18:29 +0000

    a finir

--- TP/TPACDA/exo6/Requete.java
+++ TP/TPACDA/exo6/Requete.java
@@ -3,43 +3,31 @@ import java.sql.*;
 public class Requete 
 {
 
-	public Requete()
-	{
+/*
 		
-	try
-	{
+		PreparedStatement pst = cnx.prepareStatement("SELECT Module.codes, Champ.codes FROM Champ,Module WHERE Champ.id = Module.id");
+		ResultSet rs = pst.executeQuery();
+		*/
 
-		Connection cnx = DriverManager.getConnection("jdbc:mariadb://dwarves.iut-fbleau.fr/saleh","saleh", "aladin.saleh");
+
+	public int nbrModule()//Récupere le nombre de module dans la db
+	{
+		int countMdl = 0;
+		try
+		{
+			Connection cnx = DriverManager.getConnection("jdbc:mariadb://dwarves.iut-fbleau.fr/saleh","saleh", "aladin.saleh");
 		Class.forName("org.mariadb.jdbc.Driver");
 
 		
-		PreparedStatement pst = cnx.prepareStatement("SELECT Champ.codes,Module.codes FROM Champ,Module WHERE Champ.id = Module.id");
+		PreparedStatement pst = cnx.prepareStatement("SELECT COUNT(codes) FROM Module");
 		ResultSet rs = pst.executeQuery();
 
+		while(rs.next())
+		countMdl =  rs.getInt(1);
+		System.out.println(countMdl);
+		}
 
-
-
-		while(rs.next()) {
-  		
-  		
-			System.out.println("Module : " + rs.getString(2) + "| Code : " + rs.getString(1));
-  	
-
-
-
-	}
-
-
-
-
-
-	
-
-
-
-	}
-
-	catch(ClassNotFoundException e)
+		catch(ClassNotFoundException e)
 	{
 
 	}
@@ -50,7 +38,71 @@ public class Requete
 			e.printStackTrace();
 	}
 
+	return countMdl;
+
+		
+		
 	}
 
 
+
+
+	public String Module(/*int nbrModule*/)
+	{
+		String mdl = "";
+		int i = 1;
+		try
+		{
+
+		Connection cnx = DriverManager.getConnection("jdbc:mariadb://dwarves.iut-fbleau.fr/saleh","saleh", "aladin.saleh");
+		Class.forName("org.mariadb.jdbc.Driver");
+
+		
+		PreparedStatement pst = cnx.prepareStatement("SELECT Module.codes FROM Champ,Module WHERE Champ.id = Module.id");
+		ResultSet rs = pst.executeQuery();
+
+
+
+
+		while(rs.next()) 
+		{
+
+		System.out.println("Module : " + rs.getString(2));
+		mdl = rs.getString(1);
+
+		}
+	}
+
+	catch(SQLException e)
+	{
+		e.printStackTrace();
+	}
+
+	catch(ClassNotFoundException e)
+	{
+		e.printStackTrace();
+	}
+
+
+	return mdl;
+
+	}
+
+
+
+
+
+
+
+
+
+	
+
 }
+
+	
+
+
+	
+
+
--- TP/TPACDA/exo6/Screen.java
+++ TP/TPACDA/exo6/Screen.java
@@ -11,8 +11,24 @@ public class Screen extends JFrame
 		this.setLocation(100,100);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		JPanel pan = new JPanel();
+		this.add(pan);
 
 		Requete sql = new Requete();
+
+		sql.nbrModule();
+		
+		
+
+			sql.Module();
+			
+		
+
+		JLabel[] mdl = new JLabel[3];
+		
+
+
+		
 		
 
 		this.setVisible(true);
