9d2d9b6642288837b0ad215e42cd91f7c491ab63
saleh <saleh@localhost>
2020-09-24 09:13:32 +0000

    ok

---
+++ TP/TPACDA/exo7/src/fr/iutfbleau/projet/Main.java
@@ -0,0 +1,10 @@
+package fr.iutfbleau.projet;
+
+public class Main 
+{
+
+	public static void main(String[] args) {
+		Screen fen = new Screen();
+	}
+
+}
---
+++ TP/TPACDA/exo7/src/fr/iutfbleau/projet/Screen.java
@@ -0,0 +1,43 @@
+package fr.iutfbleau.projet;
+
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel; 
+import javax.swing.ImageIcon; 
+import javax.swing.JDialog;
+
+
+public class Screen extends JFrame  
+{
+
+	
+	public Screen()
+	{
+
+		this.setSize(500,500);
+		this.setLocation(100,100);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+  
+  		 
+
+
+		JLabel image = new JLabel(new ImageIcon("res/img/3.jpg"));
+		image.setOpaque(true);
+
+
+	
+
+		this.add(image);
+		
+
+
+
+
+		Souris souris = new Souris(this,image);
+		this.addMouseListener(souris);
+
+		this.setVisible(true);
+	}
+
+}
